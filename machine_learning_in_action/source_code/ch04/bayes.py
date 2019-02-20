0f6e67e00c097dd41ddab42056f9c63ca8eb5997
gunix <gzg1211@foxmail.com>
2019-02-20 18:31:39 +0800

    update machine learning in action ch04

--- machine_learning_in_action/source_code/ch04/bayes.py
+++ machine_learning_in_action/source_code/ch04/bayes.py
@@ -1,3 +1,4 @@
+from numpy import *
 
 def loadDataSet():
     postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
@@ -27,6 +28,24 @@ def setOfWords2Vec(vocabList, inputSet):
             print("the word: %s is not in my Vocabulary!" % word)
     return returnVec
 
+def trainNB0(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory)/float(numTrainDocs)
+    p0Num = zeros(numWords)
+    p1Num = zeros(numWords)
+    p0Denom = 0.0
+    p1Denom = 0.0
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Num += trainCategory[i]
+            p1Denom += sum(trainCategory[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+    p1Vect = p1Num/p1Denom
+    p0Vect = p0Num/p0Denom
+    return p0Vect, p1Vect, pAbusive
 
 
 
